6c87a745afea7da71a032dc7b48539735d960262
FelipeSdS <fe-lipe97@outlook.com>
2020-08-21 07:15:49 -0300

    Adicionando dataCriacao ao model

--- src/main/java/br/com/model/Usuario.java
+++ src/main/java/br/com/model/Usuario.java
@@ -1,5 +1,7 @@
 package br.com.model;
 
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -28,4 +30,7 @@ public class Usuario {
 	
 	@Column(name="tx_senha")
 	private String senha;
+	
+	@Column(name="dt_criacao")
+	private Date dataCriacao;
 }
